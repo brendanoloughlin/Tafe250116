9e9bc2469be8382836617783eb0590e7dc002b5b
001128593 <brendan.oloughlin@student.tafesa.edu.au>
2025-03-15 17:43:41 +1100

    Modified files to resolve conflicts

---
+++ UniversalCalculator/MortgageCalculator.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+
+	public sealed partial class MortgageCalculator : Page
+	{
+
+		public MortgageCalculator()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Calculate(object sender, RoutedEventArgs e)
+		{
+			/// M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]
+			/// P = principal loan amount
+			/// i = monthly interest rate
+			/// n = number of months required to repay the loan
+			int principal = 0;
+			double yearlyRate = 0.0;
+			double monthlyRate = 0.0;
+			double monthlyRepayment = 0.0;
+			int payments;
+
+			if (principalBorrowBox.Text == "") 
+			{
+				return;
+			}
+			else if (yearsBox.Text == "")
+			{
+				return;
+			}
+			else if (monthsBox.Text == "")
+			{
+				return;
+			}
+			else if (yearlyRateBox.Text == "")
+			{
+				return;
+			}
+			else
+			principal = int.Parse(principalBorrowBox.Text);
+			payments = (int.Parse(yearsBox.Text) * 12) + int.Parse(monthsBox.Text);
+			yearlyRate = double.Parse(yearlyRateBox.Text) / 100;
+			monthlyRate = yearlyRate / 12;
+			monthlyRepayment = Math.Round(principal * (monthlyRate * Math.Pow(1 + monthlyRate, payments)) /
+				(Math.Pow(1 + monthlyRate, payments) - 1), 3);
+
+			monthlyRateBox.Text = Convert.ToString(Math.Round(monthlyRate, 4)) + "%";
+			var monthlyRepaymentStr = string.Format("{0:N}", monthlyRepayment);
+			monthlyRepaymentBox.Text = monthlyRepaymentStr;
+		}
+
+		private void Exit(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+    }
+}
---
+++ UniversalCalculator/TripCalculatorPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Universal calculator page for determining the amount
+	/// to pay for a given day trip.
+    /// </summary>
+    public sealed partial class TripCalculatorPage : Page
+    {
+		public TripCalculatorPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private void DateTodayClickEvent(object sender, RoutedEventArgs e)
+		{
+			dateField.Date = DateTimeOffset.Now;
+		}
+
+		private void ResetButtonClick(object sender, RoutedEventArgs e)
+		{
+			dateField.Date = null;
+			startingKmsField.Text = "";
+			endingKmsField.Text = "";
+			hiredDaysField.Text = "";
+			pricePerDayField.Text = "";
+			amountPayField.Text = "";
+		}
+
+		private async void CalculateButtonClick(object sender, RoutedEventArgs e)
+		{
+			if (!dateField.Date.HasValue
+				|| string.IsNullOrWhiteSpace(startingKmsField.Text)
+				|| string.IsNullOrWhiteSpace(endingKmsField.Text)
+				|| string.IsNullOrWhiteSpace(hiredDaysField.Text)
+				|| string.IsNullOrWhiteSpace(pricePerDayField.Text))
+			{
+				await ShowMessageDialog("Please ensure all fields are completed").ShowAsync();
+				return;
+			}
+
+			if (!int.TryParse(startingKmsField.Text, out _))
+			{
+				await ShowMessageDialog("Please enter a whole number into the Starting Kms field").ShowAsync();
+				return;
+			}
+
+			if (!int.TryParse(endingKmsField.Text, out _))
+			{
+				await ShowMessageDialog("Please enter a whole number into the Ending Kms field").ShowAsync();
+				return;
+			}
+
+			if (!int.TryParse(hiredDaysField.Text, out var hiredDays))
+			{
+				await ShowMessageDialog("Please enter a whole number into the Days Hired field").ShowAsync();
+				return;
+			}
+
+			if (!double.TryParse(pricePerDayField.Text, out var pricePerDay))
+			{
+				await ShowMessageDialog("Please enter a valid price into the Price p/Day field").ShowAsync();
+				return;
+			}
+
+			amountPayField.Text = string.Format("{0:C2}", hiredDays * pricePerDay);
+
+		}
+
+		private async void ValidateInt(object sender, RoutedEventArgs e)
+		{
+			if (sender.GetType() != typeof(TextBox))
+				return;
+
+			if (!int.TryParse(((TextBox)sender).Text, out _))
+			{
+				await ShowMessageDialog($"{((TextBox)sender).Text} is invalid. Please enter a whole number into the {((TextBox)sender).Name}").ShowAsync();
+				((TextBox)sender).Text = "";
+				return;
+			}
+		}
+
+		private async void ValidateDouble(object sender, RoutedEventArgs e)
+		{
+			if (sender.GetType() != typeof(TextBox))
+				return;
+
+			if (!double.TryParse(((TextBox)sender).Text, out _))
+			{
+				await ShowMessageDialog($"{((TextBox)sender).Text} is invalid. Please enter a decimal number into the {((TextBox)sender).Name}").ShowAsync();
+				((TextBox)sender).Text = "";
+				return;
+			}
+		}
+
+		private void ExitButtonClick(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(MainMenu));
+		}
+
+		private MessageDialog ShowMessageDialog(string message, string title = "Input Validation")
+		{
+			return new MessageDialog(message, title);
+		}
+	}
+}
